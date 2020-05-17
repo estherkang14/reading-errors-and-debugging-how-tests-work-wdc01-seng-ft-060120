bf134c0dafa5bff27030a452b169fcc3b678ba9c
Esther Kang <estherkang14@gmail.com>
2020-05-17 04:22:52 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 14 
 second_number = 127 
 sum = first_number + second_number 
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
